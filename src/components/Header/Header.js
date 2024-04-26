172307be00c7eae44c8700f602d7f2d03c47d17b
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-04-26 20:14:13 +0300

    header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,12 @@ import logo from "../../images/Logo.svg";
 import "./Header.css";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-import UserMenu from "../UserMenu/UserMenu";
-import LoginMenu from "../LoginMenu/LoginMenu";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Header(props) {
+	const currentUser = useContext(CurrentUserContext);
+
     return (
       <header className="header">
         <NavLink className="header__link" to="/">
@@ -22,10 +24,47 @@ function Header(props) {
             icon-right="C"
         />
         { props.isLoggedIn ?
-            <UserMenu
-                openModalHandler={props.openModalHandler}
-            /> :
-            <LoginMenu openModalHandler={props.openModalHandler}/>
+            (
+                <div className="header__user-menu">
+                    <button 
+                            className="header__button"
+                            onClick={() => { 
+                                    props.openModalHandler("add");
+                            }}
+                    >
+                            + Add clothes
+                    </button>
+                    
+                    <NavLink className="header__link" to="/profile">
+                        <div className="header__user">
+                            <p className="header__user-name">
+                                {currentUser.name}
+                            </p>
+                            <img className="header__user-avatar"
+                                src={currentUser.avatar}
+                                alt={currentUser.name[0]}
+                            ></img>
+                        </div>
+                    </NavLink>
+                </div>
+            ) :
+            (
+                <div className="header__login-menu">
+                    <button className="header__button"
+                    onClick={() => { 
+                            props.openModalHandler("signup");
+                    }}>
+                        Sign Up
+                    </button>
+                    
+                    <button className="header__button"
+                    onClick={() => { 
+                            props.openModalHandler("login");
+                    }}>
+                        Log in
+                    </button>
+                </div>
+            )
         }
       </header>
     );
